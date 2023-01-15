53b84ef37d5d7acbbf50e7155010326c52484ffb
Exerra <exerra@exerra.xyz>
2023-01-15 20:44:38 +0200

    modularised steam api stuff

---
+++ src/util/steam.ts
@@ -0,0 +1,76 @@
+const { PUBLIC_STEAM_KEY, PUBLIC_STEAM_USER, PROD } = import.meta.env
+
+interface GamesResponse {
+    response: {
+        game_count: number,
+        games: Array<{
+            appid: number,
+            playtime_forever: number,
+            playtime_windows_forever: number,
+            playtime_mac_forever: number,
+            playtime_linux_forever: number,
+            rtime_last_played: number,
+        }>
+    }
+}
+
+interface AchievementsResponse {
+    playerstats: {
+        steamID: string,
+        gameName: string,
+        achievements: Array<{
+            apiname: string,
+            achieved: 0 | 1,
+            unlocktime: number 
+        }>,
+        success: boolean
+    }
+}
+
+const getOwnedGames = async () => {
+    const games = await (
+        await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${PUBLIC_STEAM_KEY}&steamid=${PUBLIC_STEAM_USER}&include_played_free_games=true`)
+    ).json()
+
+    return games as GamesResponse
+}
+
+const getGameAchievements = async (appid: number) => {
+    const ftch = await fetch(`http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?key=${PUBLIC_STEAM_KEY}&steamid=${PUBLIC_STEAM_USER}&appid=${game.appid}`)
+
+	const data = await ftch.json()
+
+    return data as AchievementsResponse
+}
+
+const calculatePlaytime = async () => {
+    const games = await getOwnedGames()
+
+    let playtime = 0
+
+    games.response.games.forEach(game => {
+        playtime = playtime + game.playtime_forever
+    });
+
+    return playtime
+}
+
+const calculateAchievementCount = async () => {
+    const games = await getOwnedGames()
+
+    let achievements = 0
+
+    games.response.games.forEach(async game => {
+        const { playerstats } = await getGameAchievements(game.appid)
+
+        if (!playerstats.success) return
+
+        if (!("achievements" in playerstats)) return
+        
+        achievements = achievements + playerstats.achievements.length
+    })
+
+    return achievements
+}
+
+export { getOwnedGames, getGameAchievements, calculateAchievementCount, calculatePlaytime }
