356c336e298e06cbc48473a970b3d6803dbd4322
Exerra <exerra@exerra.xyz>
2023-01-15 18:53:34 +0200

    modularised mobile social bar & removed link underline css

---
+++ src/data/work.ts
@@ -0,0 +1,40 @@
+const work = [
+	{
+		date: "2022",
+		name: "Utilities",
+		description: "Various utilities that might come in handy",
+		url: "https://utilities.exerra.xyz"
+	},
+	{
+		date: "2022",
+		name: "Shortener",
+		description: "Quick and secure URL service integrating Exerra Phishing Check for unparalleled user protection",
+		url: "https://s.exerra.xyz"
+	},
+	{
+		date: "2022",
+		name: "Benchmarks",
+		description: "GPU benchmarks for games ranging from indie platformers to AAA shooters.",
+		url: "https://benchmarks.exerra.xyz"
+	},
+	{
+		date: "2022",
+		name: "Mod viewer",
+		description: "Every game mod Exerra has published resides here",
+		url: "https://mods.exerra.xyz"
+	},
+	{
+		date: "2022",
+		name: "Phishing API",
+		description: "API for checking if links are known phishing attempts",
+		url: "https://rapidapi.com/Exerra/api/exerra-phishing-check"
+	},
+	{
+		date: "Since 2019",
+		name: "Karen Bot",
+		description: "Multi-purpose Discord bot featuring commands for server moderation, sub-reddits, Karen Bot profiles, Spotify songs/albums/artists and more",
+		url: "https://karen.exerra.xyz"
+	}
+]
+
+export default work
