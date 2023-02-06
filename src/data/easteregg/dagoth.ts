4c8970368cb3f44a6e212f22864c325fd12b09b4
Exerra <exerra@exerra.xyz>
2023-02-06 19:57:21 +0200

    progress
    now all thats needed is better dialogue

---
+++ src/data/easteregg/dagoth.ts
@@ -0,0 +1,14 @@
+const introduction = {
+    reply: "Introductionary text",
+    audio: "introduction.mp3"
+}
+
+const userQuestions = [
+    {
+        question: "What country are you from?",
+        reply: "Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia Latvia ",
+        audio: "latvia.mp3"
+    }
+]
+
+export { introduction, userQuestions }
