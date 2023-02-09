31847beaf889483cd170cb59308b148d6cffcaa1
Exerra <exerra@exerra.xyz>
2023-02-09 19:02:07 +0200

    more naaawledge

--- src/data/knowledge.ts
+++ src/data/knowledge.ts
@@ -11,12 +11,12 @@ const knowledge = [
 	},
 	{
 		type: "Language",
-		name: "Javascript",
+		name: "JavaScript",
 		used: "write the interactive parts of a website"
 	},
 	{
 		type: "Superset",
-		name: "Typescript",
+		name: "TypeScript",
 		used: "create applications with more type safety than Javascript"
 	},
 	{
@@ -39,6 +39,28 @@ const knowledge = [
 		name: "Express",
 		used: "create APIs"
 	},
+	{
+		type: "Almost a meta-framework",
+		name: "Astro",
+		used: "create fast websites using any framework you want"
+	},
+	{
+		type: "Software suite",
+		name: "Firebase",
+		used: "create databases, manage user authentication, host projects and much more"
+	},
+	{
+		type: "Database",
+		name: "PostgreSQL",
+		used: "applications that require more data types and inherited properties",
+		override: "for"
+	},
+	{
+		type: "Content Management System",
+		name: "Storyblok",
+		used: "manage content, allowing multiple contributors to create, edit and publish",
+		override: "for"
+	}
 ]
 
 export default knowledge
