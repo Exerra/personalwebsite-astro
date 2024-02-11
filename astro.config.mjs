490ae7bfade38f3aac61909bd3c1dccde12aaaad
Exerra <exerra@exerra.xyz>
2024-02-11 18:52:10 +0200

    removed useless deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://exerra.xyz"
